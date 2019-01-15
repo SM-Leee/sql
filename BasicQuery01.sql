91d323fe73cea6395e9a88205ab13359127a16fe
BIT <BIT@DESKTOP-E23PFRN>
2019-01-15 09:37:52 +0900

    190115 09:37 basic query select

--- BasicQuery01.sql
+++ BasicQuery01.sql
@@ -15,3 +15,15 @@ desc pet;
 insert into pet values('두부','이성민','dog','M','2015-2-27',NULL);
 
 select * from pet;
+
+-- load data 파일을 불러와서 database에 한번에 insert 하는 방법(\n은 null을 뜻한다)
+load data local infile "D:\\duzon\\eclipse-workspace\\sql\\pet.txt" into table pet;
+
+-- sql select
+select * from pet; -- database의 모든 table을 다 가져올때;
+select * from pet where name = 'Bowser'; -- Bowser라는 이름을 가진 data 만 가져오시오
+select * from pet where birth >= '1998-01-01';	-- 출생년도가 1998년 이상인 data 만 가져오시오
+select * from pet where species = 'dog' and gender = 'f';	-- 종이 강아지이고 성별이 암컷인 동물의 data를 가져오시오.
+select * from pet where species='snake' or species ='bird'; -- 종이 뱀이거나 새인 data를 가져오시오.
+
+select name, birth from pet;	-- name 과 birth의 data만 가져오시오.
