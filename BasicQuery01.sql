58d8cf15e1153646a5fb6a2ceffa70c891ee12cf
BIT <BIT@DESKTOP-E23PFRN>
2019-01-15 11:24:10 +0900

    190115 11:23 Basic Query select4

--- BasicQuery01.sql
+++ BasicQuery01.sql
@@ -30,9 +30,23 @@ select name, birth from pet;	-- name 과 birth의 data만 가져오시오.
 select name, birth from pet order by birth desc;	-- order by 순서대로 정리하고 싶을때에 사용된다. (desc - 내림차순 / asc - 오름차순(default))
 select name, gender, birth from pet order by gender asc, birth desc;
 
--- null 다루기
+-- null 다루기 (is null, is not null)
 select * from pet where gender is null;
 
 select * from pet where gender is not null;
 
+-- 패턴매칭(Like 검색) (like, not like)
+select * from pet where name like "b%";	-- name이 b로 시작하는 모든 문자열
+select * from pet where name like "b____"; -- name이 b로 시작하고 문자열이 5개로 이루어진 data를 가져오시오.
+select * from pet where name like "%fy";
+select * from pet where name like "%w%";
+select * from pet where name like "_____"; -- name에서 문자열의 갯수가 5인 data를 가져오시오
+select * from pet where length(name)=5; -- name에서 문자열의 갯수가 5인 data를 가져오시오
+
+-- count()
+select count(*) as '총 애완동물 수' from pet;
+
+select owner, count(*) from pet group by owner; -- 각 주인들이 몇마리의 애완동물을 가지고 있는가를 알고싶을때
+
+select owner, count(*) from pet where death is null or death ='0000-00-00' group by owner having count(*)>1; 
 
