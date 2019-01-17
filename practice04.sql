789bd09b1fc745da8438fc292b0cbad130aa2cf5
BIT <BIT@DESKTOP-E23PFRN>
2019-01-18 08:57:21 +0900

    190118 08:57 ptactice4 4번문제 푸는중

--- practice04.sql
+++ practice04.sql
@@ -45,10 +45,14 @@ select c.dept_no,avg(salary) as avg_salary
 			where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01'
             group by c.dept_no;
     
-    
-    
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
+select *
+	from employees a, 
+
+select b.dept_no, c.dept_name
+	from dept_manager b, departments c
+    where b.dept_no = c.dept_no and b.to_date = '9999-01-01';
 
 
 -- 문제5.
