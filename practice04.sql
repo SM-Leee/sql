bcc31a5b4cc267f9e85d6f0ee523be55d007f185
BIT <BIT@DESKTOP-E23PFRN>
2019-01-17 16:57:57 +0900

    190117 16:57 문제2 complete

--- practice04.sql
+++ practice04.sql
@@ -11,26 +11,30 @@ select count(*) as '평균 연봉보다 많은 월급을 받는 직원의수'
 
 -- 문제2. 
 -- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. 
--- 보류
-select a.emp_no as '사번', concat(a.first_name,' ',a.last_name) as '이름', c.dept_name as '부서', d.max_salary as '연봉'
-	from employees a, dept_no b,departments c, 
-		(select a.emp_no, max(c.salary) as max_salary
+
+select a.emp_no as '사번', concat(a.first_name,' ',a.last_name) as '이름', d.dept_name as '부서', b.salary as '연봉'
+	from employees a, salaries b, dept_emp c, departments d,
+		(select b.dept_no, max(c.salary) as max_salary
 			from employees a, dept_emp b, salaries c
 			where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date ='9999-01-01' and c.to_date='9999-01-01'
-			group by b.dept_no) d
-	where a.emp_no = d.emp_no and a.emp_no = b.emp_no and b.dept_no = c.dept_no and b.to_date ='9999-01-01';
+			group by b.dept_no) e
+	where a.emp_no = b.emp_no and a.emp_no =c.emp_no and c.dept_no = d.dept_no and c.dept_no = e.dept_no
+		and b.to_date = '9999-01-01' and c.to_date = '9999-01-01' and b.salary = e.max_salary
+	group by c.dept_no
+    order by e.max_salary desc;
 
-select a.emp_no, max(c.salary) as max_salary
+select b.dept_no, max(c.salary) as max_salary
 	from employees a, dept_emp b, salaries c
 	where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date ='9999-01-01' and c.to_date='9999-01-01'
 	group by b.dept_no;
 
 
 -- 문제3.
--- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요 
+-- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요
+-- 보류 
 select a.emp_no as '사번', concat(a.first_name,' ',a.last_name) as '이름', b.salary as '연봉'
 	from employees a, salaries b, dept_emp c, 
-		(select c.dept_no, avg(salary)as avg_salary
+		(select c.dept_no, avg(salary) as avg_salary
 			from employees a, salaries b, dept_emp c
 			where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01'
             group by c.dept_no) d
@@ -38,7 +42,7 @@ select a.emp_no as '사번', concat(a.first_name,' ',a.last_name) as '이름', b
     and b.salary > d.avg_salary
     group by c.dept_no;
 
-select a.emp_no, c.dept_no,avg(salary)as avg_salary
+select c.dept_no,avg(salary) as avg_salary
 			from employees a, salaries b, dept_emp c
 			where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01'
             group by c.dept_no;
