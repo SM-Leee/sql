cbbd79c914a6ebde528f4d566026604ed87eb350
BIT <BIT@DESKTOP-E23PFRN>
2019-01-18 17:46:41 +0900

    190118 17:46 practice4 complete

--- practice04.sql
+++ practice04.sql
@@ -53,7 +53,7 @@ select a.emp_no as '사번', concat(a.first_name,' ',a.last_name) as '이름', d
 		from employees a, dept_manager b, departments c
 		where a.emp_no = b.emp_no and b.dept_no = c.dept_no and b.to_date = '9999-01-01') d
 	where a.emp_no = b.emp_no and b.dept_no = c.dept_no and b.dept_no = d.dept_no and b.to_date = '9999-01-01';
-
+    
 select b.dept_no, c.dept_name, concat(a.first_name,' ',a.last_name) as name
 	from employees a, dept_manager b, departments c
     where a.emp_no = b.emp_no and b.dept_no = c.dept_no and b.to_date = '9999-01-01';
@@ -62,12 +62,79 @@ select b.dept_no, c.dept_name, concat(a.first_name,' ',a.last_name) as name
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
 
+select a.emp_no as '사번', concat(a.first_name,' ',a.last_name) as '이름', c.title as '직책', b.salary as '연봉'
+	from employees a, salaries b, titles c, dept_emp d,
+		(select c.dept_no, max(b.salary)
+			from employees a, salaries b, dept_emp c
+			where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date = '9999-01-01' and c.to_date = '9999-01-01') e
+	where a.emp_no=b.emp_no and a.emp_no=c.emp_no and a.emp_no=d.emp_no and d.dept_no=e.dept_no
+	and b.to_date='9999-01-01' and c.to_date='9999-01-01' and d.to_date='9999-01-01'
+    order by b.salary desc;
+
+select c.dept_no, max(b.salary)
+	from employees a, salaries b, dept_emp c
+    where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date = '9999-01-01' and c.to_date = '9999-01-01';
+
 -- 문제6.
--- 평균 연봉이 가장 높은 부서는? 
+-- 평균 연봉이 가장 높은 부서는?
+select a.dept_name
+	from (select b.dept_name, max(c.avg_salary)
+			from dept_emp a, departments b,
+			(select c.dept_no, avg(b.salary) as avg_salary
+				from employees a, salaries b, dept_emp c
+				where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date = '9999-01-01' and c.to_date='9999-01-01'
+				group by c.dept_no) c
+			where a.dept_no = b.dept_no and a.dept_no = c.dept_no and a.to_date='9999-01-01') a;
+
+select b.dept_name, max(c.avg_salary)
+	from dept_emp a, departments b,
+	(select c.dept_no, avg(b.salary) as avg_salary
+		from employees a, salaries b, dept_emp c
+		where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date = '9999-01-01' and c.to_date='9999-01-01'
+		group by c.dept_no) c
+	where a.dept_no = b.dept_no and a.dept_no = c.dept_no and a.to_date='9999-01-01';
+ 
+select c.dept_no, avg(b.salary) as avg_salary
+	from employees a, salaries b, dept_emp c
+    where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date = '9999-01-01' and c.to_date='9999-01-01'
+	group by c.dept_no;
 
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
+select a.title
+	from (select b.title ,max(c.avg_salary)
+			from employees a, titles b, 
+				(select c.title, avg(b.salary) as avg_salary
+				from employees a, salaries b, titles c
+				where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01'
+				group by c.title) c
+			where a.emp_no = b.emp_no and b.title = c.title and b.to_date ='9999-01-01') a;
+
+select b.title ,max(c.avg_salary)
+	from employees a, titles b, 
+    (select c.title, avg(b.salary) as avg_salary
+		from employees a, salaries b, titles c
+		where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01'
+		group by c.title) c
+	where a.emp_no = b.emp_no and b.title = c.title and b.to_date ='9999-01-01';
+
+select c.title, avg(b.salary) as avg_salary
+	from employees a, salaries b, titles c
+    where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01'
+    group by c.title;
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
+
+select d.dept_name as '부서이름', concat(a.first_name,' ',last_name) as '이름', b.salary as '연봉', e.manager_name as '매니저 이름', e.manager_salary as '매니저 연봉'
+	from employees a, salaries b, dept_emp c, departments d,
+    (select concat(a.first_name,' ',last_name) as manager_name, c.dept_no, b.salary as manager_salary
+		from employees a, salaries b, dept_manager c
+		where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01') e
+	where a.emp_no=b.emp_no and a.emp_no=c.emp_no and c.dept_no = d.dept_no and e.dept_no=c.dept_no
+    and b.salary > e.manager_salary and b.to_date = '9999-01-01' and c.to_date='9999-01-01';
+
+select a.emp_no, c.dept_no, b.salary as manager_salary
+	from employees a, salaries b, dept_manager c
+    where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.to_date='9999-01-01' and c.to_date='9999-01-01';
